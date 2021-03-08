7e30ea7323b12dd148d22441e5175bfb641607b1
Clément Terrien <clement.terrien@hotmail.fr>
2021-03-08 15:53:06 +0100

    debugging managerRegistry bundle

--- MyMovieList/src/Repository/MovielistRepository.php
+++ MyMovieList/src/Repository/MovielistRepository.php
@@ -4,7 +4,7 @@ namespace App\Repository;
 
 use App\Entity\Movielist;
 use Doctrine\Bundle\DoctrineBundle\Repository\ServiceEntityRepository;
-use Doctrine\Common\Persistence\ManagerRegistry;
+use Doctrine\Persistence\ManagerRegistry;
 
 /**
  * @method Movielist|null find($id, $lockMode = null, $lockVersion = null)
